ed7a6263092d4d4eb4d4fa4ab3cc3b256f0e1549
xmeix <lamiaboualouache@gmail.com>
2023-10-04 17:06:32 +0100

    setting the routes

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt import views as jwt_views
+
 
 urlpatterns = [
-    # path('login/',views.user_login,name='login'),  
-    # path('register/',views.register,name='register'),  
-    # path('logout/',views.user_logout,name='logout'),  
+    path('login/',views.user_login,name='login'),  
+    path('register/',views.register,name='register'),  
+    path('logout/',views.user_logout,name='logout'),  
     # path('addticket/',views.addticket,name='addticket'),  
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh')
 ]

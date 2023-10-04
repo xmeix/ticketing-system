697f4106f69e6848ca41396ca1d6e3a0b9731a97
xmeix <lamiaboualouache@gmail.com>
2023-10-05 00:27:19 +0100

    add: navbar design

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import "./Navbar.css";
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <h1>Navbar</h1>
+    <div className="navbar flex-row justify-between align-center">
+      <div className="nav-title">TicketTrac</div>
+      <div className="nav-params flex-row align-center">
+        <div className="nav-user">#boualouache lamia</div>
+        <button>logout</button>
+      </div>
     </div>
   );
 };

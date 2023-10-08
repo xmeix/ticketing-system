9343f0e850d0ede9c1ae44b508dafdc6fd816e26
xmeix <lamiaboualouache@gmail.com>
2023-10-08 05:10:11 +0100

    handle login in multiple tabs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom"; 
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/loginPage/LoginPage";
 import Navbar from "./components/Navbar/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import RegisterPage from "./pages/admin/RegisterPage";
 import { Toaster } from "react-hot-toast";
@@ -10,6 +10,28 @@ import Home from "./pages/home/Home";
 
 function App() {
   const { isLoggedIn, user } = useSelector((state) => state.auth);
+
+  // ___________REFRESH_IN_ALL_TABS:LOGIN/REGISTER_CASES_________________________________________
+
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
+  const handleStorageChange = async (event) => {
+    if (event.key === "isLoggedIn") {
+      handleRefresh();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
+  // ____________________________________________________________________
 
   return (
     <>
